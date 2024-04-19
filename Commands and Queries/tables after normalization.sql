3521902edf2db62e5586fa2657e2e4601d6045db
Sanjay Ponnambalam <113366933+Sanjayponnambalam@users.noreply.github.com>
2024-04-19 22:46:46 +0530

    Update tables after normalization.sql

--- Commands and Queries/tables after normalization.sql
+++ Commands and Queries/tables after normalization.sql
@@ -18,3 +18,25 @@ VALUES
     (8, 'Media Advertising', 'General Public'),
     (9, 'Policy Speeches', 'General Public'),
     (10, 'Voter Turnout Push', 'General Public');
+
+
+-- Second table
+-- Create the campaigndates table
+CREATE TABLE campaigndates (
+    StartDate DATE,
+    EndDate DATE
+);
+
+-- Insert the values into the campaigndates table
+INSERT INTO campaigndates (StartDate, EndDate)
+VALUES
+    ('2024-03-01', '2024-05-01'),
+    ('2024-04-15', '2024-06-15'),
+    ('2024-05-10', '2024-07-10'),
+    ('2024-06-20', '2024-08-20'),
+    ('2024-07-25', '2024-09-25'),
+    ('2024-08-30', '2024-10-30'),
+    ('2024-09-15', '2024-11-15'),
+    ('2024-10-20', '2024-12-20'),
+    ('2024-11-05', '2025-01-05'),
+    ('2024-12-10', '2025-02-10');
